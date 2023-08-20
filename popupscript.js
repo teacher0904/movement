8c136b4f5439bdae7d987bc338ad94ed44b075e3
0904teacher <0729teachers@gmail.com>
2023-08-20 09:44:38 +0900

    main

--- popupscript.js
+++ popupscript.js
@@ -1,45 +1,54 @@
-const closeBtn = document.getElementById("close-btn");
-const dontShowBtn = document.getElementById("dont-show-btn");
+    const closeBtn = document.getElementById("close-btn");
+    const dontShowBtn = document.getElementById("dont-show-btn");
+    const storageKey = "hidePopup";
+    const hidePopupExpiration = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
 
-function setCookie(name, value, days) {
-  const expires = new Date();
-  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-  document.cookie = name + '=' + value + ';expires=' + expires.toUTCString() + ';path=/';
-}
+    function setLocalStorageWithExpiration(name, value, time) {
+      const storageObject = {
+        value: value,
+        expires: new Date().getTime() + time,
+      };
+      localStorage.setItem(name, JSON.stringify(storageObject));
+    }
+  
+    function getLocalStorageWithExpiration(name) {
+      const storedObject = JSON.parse(localStorage.getItem(name));
+      if (!storedObject) return null;
 
-function getCookie(name) {
-  const value = '; ' + document.cookie;
-  const parts = value.split('; ' + name + '=');
-  if (parts.length === 2) {
-    return parts.pop().split(';').shift();
-  }
-}
+      const now = new Date().getTime();
+      if (storedObject.expires > now) {
+        return storedObject.value;
+      } else {
+        localStorage.removeItem(name);
+        return null;
+      }
+    }
+  
+    function hidePopup() {
+      const popup = document.getElementById("popup");
+      if (popup) {
+        popup.style.display = "none";
+      }
+    }
 
-function hidePopup() {
-  const popup = document.getElementById("popup");
-  if (popup) {
-    popup.style.display = "none";
-  }
-}
-
-function showPopup() {
-  const popup = document.getElementById("popup");
-  if (popup) {
-    popup.style.display = "block";
-  }
-}
-
-dontShowBtn.addEventListener("click", () => {
-  setCookie("hidePopup", true, 1);
-  hidePopup();
-});
-
-closeBtn.addEventListener("click", () => {
-  hidePopup();
-});
-
-const isHidePopup = getCookie('hidePopup') === 'true';
-
-if (!isHidePopup) {
-  showPopup();
-}
+    function showPopup() {
+      const popup = document.getElementById("popup");
+      if (popup) {
+        popup.style.display = "block";
+      }
+    }
+  
+    dontShowBtn.addEventListener("click", () => {
+      setLocalStorageWithExpiration(storageKey, true, hidePopupExpiration);
+      hidePopup();
+    });
+  
+    closeBtn.addEventListener("click", () => {
+      hidePopup();
+    });
+  
+    const isHidePopup = getLocalStorageWithExpiration(storageKey);
+    
+    if (!isHidePopup) {
+      showPopup();
+    }
