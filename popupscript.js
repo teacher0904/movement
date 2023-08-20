ae8f93fcbdc0c348af43af6ddaeebfb23ede41ca
0904teacher <0729teachers@gmail.com>
2023-08-20 09:32:23 +0900

    main

--- popupscript.js
+++ popupscript.js
@@ -1,21 +1,46 @@
-const popup = document.getElementById("popup");
+
 const closeBtn = document.getElementById("close-btn");
 const dontShowBtn = document.getElementById("dont-show-btn");
-const today = new Date().toISOString().slice(0, 10);
 
-if (localStorage.getItem("hidePopup") !== today) {
-  popup.style.display = "block";
+function setCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = name + '=' + value + ';expires=' + expires.toUTCString() + ';path=/';
+}
+
+function getCookie(name) {
+  const value = '; ' + document.cookie;
+  const parts = value.split('; ' + name + '=');
+  if (parts.length === 2) {
+    return parts.pop().split(';').shift();
+  }
 }
 
 function hidePopup() {
-  popup.style.display = "none";
+  const popup = document.getElementById("popup");
+  if (popup) {
+    popup.style.display = "none";
+  }
 }
+
+function showPopup() {
+  const popup = document.getElementById("popup");
+  if (popup) {
+    popup.style.display = "block";
+  }
+}
+
+dontShowBtn.addEventListener("click", () => {
+  setCookie("hidePopup", true, 1);
+  hidePopup();
+});
 
 closeBtn.addEventListener("click", () => {
   hidePopup();
 });
 
-dontShowBtn.addEventListener("click", () => {
-  localStorage.setItem("hidePopup", today);
-  hidePopup();
-});
+const isHidePopup = getCookie('hidePopup') === 'true';
+
+if (!isHidePopup) {
+  showPopup();
+}
