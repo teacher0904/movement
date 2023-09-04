a7427183e5abdd38c92cc7dc45c64ce54c4db683
junedevkr <87560569+junedevkr@users.noreply.github.com>
2023-09-05 00:58:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -102,10 +102,6 @@ function displayDataTime(dataTime) {
 
 function randomLink() {
   var links = [
-    'https://open.kakao.com/o/gD77RlCf',
-    'https://open.kakao.com/o/g5pxyWCf', 
-    'https://open.kakao.com/o/gY6GIWCf',
-    'https://open.kakao.com/o/gvCrJWCf'
   ];
 
   var randomIndex = Math.floor(Math.random() * links.length);
